6373372cf2691ee8f8587bafd7464a0be3cd94b7
mbenitez <mbenitez.a@gmail.com>
2023-01-27 22:27:34 -0500

    Add WebpackPwaManifest configuration to generate manifest.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,6 +22,24 @@ module.exports = () => {
         template: './index.html',
         title: 'JATE Text Editor'
       }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor (JATE)',
+        short_name: 'JATE',
+        description: 'Text Editor',
+        fingerprints: false,
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
       new WorkboxPlugin.GenerateSW()     
     ],
 
